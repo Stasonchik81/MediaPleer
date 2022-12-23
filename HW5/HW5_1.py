b149e71aa8a4bf2abc38cee3306d898b18296c77
Galina094 <102922761+Galina094@users.noreply.github.com>
2022-12-23 14:04:51 +0300

    Delete HW5 directory

--- HW5/HW5_1.py
+++
@@ -1,7 +0,0 @@
-# Напишите программу, удаляющую из текста все слова, содержащие ""абв""
-
-MyString = 'I love Pythonabc andabc Java'
-NotInString = 'abc'
-
-MyString = MyString.replace('abc','')
-print(MyString)
